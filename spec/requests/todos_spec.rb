5ebe4c810076ba873a1c268afbb8855441968c66
Andy Marthin <mr.andymarthin@gmail.com>
2017-10-14 18:58:34 +0700

    initial commit

---
+++ spec/requests/todos_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Todos API', type: :request do 
+let!(:todos) { create_list(:todo, 10) }
+  let!(:todo_id) { todos.first.id  }
+  
+  # Test suite for GET /todos
+  describe 'GET /todos' do 
+    # make HTTP get request before each example
+    before { get '/todos' }
+
+    it 'returns todos'do 
+      # Note 'json' is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10) 
+    end
+
+    it 'returns status code 200' do 
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+  # Test suite for GET /todos/:id
+  decribe 'GET /todos/:id' do 
+    before { get "/todos/#{todo_id}"}
+
+    context 'when the record exists' do
+      it 'returns the todo' do
+
+
+      end
+
+    end
+  end
+
+
+
+end
